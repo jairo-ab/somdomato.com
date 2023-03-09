fef6e521f8eef44d406d2bb308ca9e550a7a7466
Lucas Saliés Brum <97509+sistematico@users.noreply.github.com>
2023-03-09 03:03:35 -0400

    Update path

--- components/Plyr.vue
+++ components/Plyr.vue
@@ -4,9 +4,7 @@ const song = ref('Carregando...')
 const player = ref(null)
 const source = ref(null)
 
-function refresh() {
-  source.value.src = `${base}/principal?ts=` + ~~(Date.now() / 1000)
-}
+function refresh() { source.value.src = `${base}/principal?ts=` + ~~(Date.now() / 1000) }
 
 function reload() {
   refresh()
@@ -17,24 +15,19 @@ function reload() {
 onMounted(() => {
   setInterval(async () => {
     const {
-      icestats: {
-        source: { title }
-      }
+      icestats: { source: { title } }
     } = await (await fetch(`${base}/json`)).json()
     song.value = title
   }, 1000)
 
   refresh()
 
-  player.value.onpause = function () {
-    refresh()
-  }
+  player.value.onpause = function () { refresh() }
 })
 </script>
 <template>
   <div class="d-flex flex-column flex-md-row align-items-center">
     {{ song }}
-
     <div class="mt-2 mt-md-0" style="display: inline-flex; align-items: center">
       <div class="plyr__controls">
         <button @click.prevent="reload()" class="plyr__controls__item plyr__control" type="button" data-plyr="play" aria-label="Pause">
@@ -44,7 +37,6 @@ onMounted(() => {
           <span class="plyr__sr-only">Reload</span>
         </button>
       </div>
-
       <vue-plyr>
         <audio ref="player" crossorigin="true" controls>
           <source ref="source" src="https://radio.somdomato.com/principal" type="audio/mp3" />
@@ -53,16 +45,3 @@ onMounted(() => {
     </div>
   </div>
 </template>
-<style scoped>
-/* .plyr__controls>button,
-.plyr__controls__item .plyr__control,
-.plyr__control {
-  color: #fff !important;
-}
-
-.plyr__control:hover,
-.plyr--audio .plyr__control {
-  background: #00b2ff;
-  color: #fff;
-} */
-</style>
