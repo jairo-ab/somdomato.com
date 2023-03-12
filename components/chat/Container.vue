479bce28ae71546b9e386e41558c760b5cb6eeab
Lucas Saliés Brum <97509+sistematico@users.noreply.github.com>
2023-03-12 14:41:34 -0400

    Added avatar assets

--- components/chat/Container.vue
+++ components/chat/Container.vue
@@ -1,11 +1,28 @@
 <template>
-  <iframe src="https://chat.somdomato.com"></iframe>
+  <div class="iframe-container">
+    <iframe src="https://chat.somdomato.com" allowfullscreen></iframe>
+  </div>
 </template>
 <style scoped>
-iframe {
+/* iframe {
   width: 100%;
   min-height: 550px;
   max-height: 100%;
   border: none;
+} */
+
+.iframe-container {
+  position: relative; /* 16:9 */
+  padding-top: 56.25%;
+  overflow: hidden;
+}
+
+.iframe-container iframe {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  border: 0;
 }
 </style>
